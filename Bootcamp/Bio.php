9d0925102d895806faeddc7136a6112717fbbd67
AngelicaGumanid <84778428+AngelicaGumanid@users.noreply.github.com>
2021-08-04 12:37:53 +0800

    Add files via upload

---
+++ Bootcamp/Bio.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title> BIO </title>
+
+</head>
+
+<style>
+ body{
+	margin: 0;
+	padding: 0;
+	}
+	
+.box{
+  width: 500px;
+  height: 700px;
+  background: black;
+  padding: 30px;
+  text-align: center;
+  margin: auto;
+  margin-top: 1%;
+  color: white;
+  font-family: 'Century Gothic',sans-serif;
+}
+
+</style>
+
+<body>
+
+<div class="box">
+
+<h1 style ="text-align: left;"> BIO </h1>
+	
+	<img src="https://i.ibb.co/X7hbfHn/profile.jpg" alt="profile" border="0" alt="" class="box-img" style="border-radius: 50%; width: 200px; height: 200px;">
+	
+<?php
+
+	echo "</br></br></br>";
+	
+	echo "<font size = 5 > <b> Angelica Rose N. Gumanid </b> </font> </br></br></br>";
+	
+	echo "<b>Age: </b> 19 <br>";
+	echo "<b>Address: </b> # 23 Mayor Tanyag St. Zone 6 South Signal Village Taguig City </br></br></br></br>";
+	
+	
+	echo "My Favorite color is Cream and </br>";
+	echo "my Favorite Food is Pizza.</br></br>";
+	echo "I am an upcoming Second Year College<br>";
+	echo "and I am studying in University of Makati<br>";
+
+
+?>
+</div>
+
+
+</body>
+</html>
